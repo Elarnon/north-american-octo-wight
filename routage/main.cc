95e84172596bcda94fad082823d433c55f536455
Kyo Dralliam <chocobodralliam@gmail.com>
2014-04-05 17:49:37 +0200

    Go Go Go In the Main git pull!

--- routage/main.cc
+++ routage/main.cc
@@ -19,53 +19,59 @@ struct CmpCar {
 };
 
 int main() {
-    parse();
-    djikstra_init();
-    dijkstra(start);
-    auto pq = std::priority_queue<long, std::vector<long>, CmpCar>();
-    for (int i(0); i < nvehic; ++i) {
-        pq.push(i);
-    }
+
+
+
+                            parse();
+                 djikstra_init     ();
+           dijkstra                 (start);
+        auto pq =                       std::priority_queue
+      <long,                         std::vector<long>, CmpCar>();
+     for(int 
+    i(0);i 
+    <nvehic;
+    ++i){
+    pq.push
+    (i);}
     // Diaspora
-    auto points = allPointsSets(DIASPORA_SIZE); // TODO
-    int diaspora_time = total_time / DIASPORA_DIV;
-    for(int i =0; i<nvehic; i++) {
-        auto l = find_one_path(points[i]);
-        std::vector<long> path{ std::begin(l), std::end(l) };
-        for(int j=1; j<path.size(); j++) {
-            if(dist[path[j]] > diaspora_time) {
-                break;
-            }
-
-            cars[i].move(path[j], dist[path[j]] - dist[path[j-1]]);
-        }
-    }
-    // Best paths
-    while (!pq.empty()) {
-        auto i = pq.top();
-        pq.pop();
-        Car &c = cars[i];
-        Rue* best(NULL);
-        double best_cost = 0;
-        for (Rue *r : inters[c.pos].alls) {
-            if (r->time + c.time < total_time) {
-                auto cst = r->gain(c.pos);
-                if (cst > -1.0) {
-                    if (best == NULL || cst > best_cost) {
-                        best_cost = cst;
-                        best = r;
-                    }
-                }
-            }
-        }
-        if (best != NULL) {
-            c.move(best->path(c.pos), best->time);
-            best->ok = true;
-            pq.push(i);
-        } 
-    }
+    auto points 
+    = allPointsSets                       (DIASPORA_SIZE); // TODO
+    int diaspora_time                           = total_time / 
+     DIASPORA_DIV;                                 for
+      (int i=0;i<nvehic;                           i++
+       ) {auto l = find_one_path               (points[i]);
+          std::vector<long>                     path{ std::
+             begin(l),std::end(l) };           for(int j=1; 
+                     j<path.size(); j++) { if(dist[path[j]] 
 
 
 
-  sin_printer(nvehic, cars) ;
-}
+                         > diaspora_time) {
+                   break;}cars[i].move(path[j],dist[path[j]]
+     -dist[path[j-1]]);                          }}// Best paths
+    while (!pq.empty                               ()) {auto i=pq.
+  top();pq.pop();                                   Car&c= cars[i];
+  Rue* best                                           (NULL); double 
+  best_cost                                           =0;for(Rue *r: 
+  inters[                                               c.pos].alls) 
+  {if(r->                                               time+c.time 
+  <total_time                                           ){auto cst= 
+  r->gain                                               (c.pos);if 
+  (cst>-1.0)                                            {if(best== 
+  NULL||cst                                             >best_cost) 
+  {best_cost                                            =cst;best=r;
+  }}}}  if                                              (best!=NULL) 
+  {c.move                                               (best->path
+  (c.pos),                                              best->time);
+   best->ok                                             =true;pq.
+    push(i);                                          }}//*****
+     sin_printer                               (nvehic, cars);
+       //########                             ##########
+          //##########                    ##########
+              //##########           ###########
+                   //######################
+                       //##############
+                              }
+
+
+
