6beaf68efa25bf43b5361d711db3bb04235379ce
Kyo Dralliam <chocobodralliam@gmail.com>
2014-04-05 17:09:56 +0200

    printer

---
+++ routage/printer.cc
@@ -0,0 +1,23 @@
+#include <cmath>
+#define PI 3.14159265
+
+void print_basic(int& c, int display) {
+  int k = floor(sin( static_cast<float>(c) * PI/80.)) ;
+  ++c;
+  for(int i = 0 ; i < k ; ++i) {
+    putchar(' ') ;
+  }
+  printf("%d", display) ;
+}
+
+void sin_printer(int nvehic, Car* cars) {
+  int x = 0 ; 
+  print_basic(x, nvehic) ;
+  for (int i(0); i < nvehic; ++i) {
+    Car &c = cars[i];
+    print_basic(x, c.path.size());
+    for (auto i : c.path) {
+      print_basic(x, i);
+    }
+  }
+}
---
+++ routage/printer.h
@@ -0,0 +1,6 @@
+#ifndef PRINTER_HEADER_GUARD
+#define PRINTER_HEADER_GUARD
+
+void sin_printer(int nvehic, Car* cars) ;
+
+#endif
