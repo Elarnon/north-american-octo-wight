91bca8e0df80fdece2c797e780b94313787a79a3
Anael Bonneton <anael.bonneton@gmail.com>
2014-04-05 12:04:57 +0000

    new diaspora : less time consuming

--- routage/diaspora.py
+++ routage/diaspora.py
@@ -1,7 +1,10 @@
 from math import *
 from floyd_warshall import *
 
-def diaspora(londre, center, costs, inters):
+
+def diaspora(londre, costs, inters):
+    centerX = 48.859800
+    centerY = 2.340953
     r2 = sqrt(2)
     kappa = 3   # power parameter for gain (odd number)
     dirs = [(1,0), (r2, r2), (0,1), (-r2, r2), (-1,0), (-r2, -r2), (0, -1), (r2, -r2)]
@@ -9,14 +12,12 @@ def diaspora(londre, center, costs, inters):
     for but in inters:
         for i in xrange(0,8):
             (x,y) = dirs[i]
-            gain0 = (x * (but.lat - center.lat) + y * (but.lon - center.lon))
+            gain0 = (x * (but.lat - centerX) + y * (but.lon - centerY))
             gain = copysign(gain0 **  kappa, gain0) / cost[londre.key][but.key]
             (prev_pos, prev_gain) = max_buts[i]
             if prev_gain < gain :
                 max_buts[i] = (but, gain)
     return [ x for (x,y) in max_buts ]
-
-
 
 def main_diaspora(londres, center, inters, rues):
     (costs, paths) = to_matrix(inters, rues)
--- routage/trivialWithDiaspora.py
+++ routage/trivialWithDiaspora.py
@@ -1,18 +1,42 @@
 from parse import *
-from diaspora import main_diaspora
 
 GAIN_MULT = 5
 
 def trivial(cars, inters, rues, time, nvehic, start, all_cars):
     cars = PriorityQueue(nvehic)
-    di = main_diaspora(start, inters, rues)
-    i = 0
-    for car in all_cars:
-        for p in di[i]:
-            car.move(p.path(car.pos), p.time)
-        i = i + 1
     for car in all_cars:
         cars.put(car)
+
+    ## TODO : improve parameter
+    diasporaTime = time / 10
+
+## First part : diaspora
+    while not cars.empty():
+        c = cars.get()
+        done = False
+        best = None
+        for r in inters[c.pos].alls:
+            best_cost = None
+            if r.time + c.time < diasporaTime:
+                cst = r.gain(c.pos)
+                if best is None:
+                    best = r
+                    best_cost = cst
+                elif cst > best_cost:
+                    best = r
+                    best_cost = cst
+        if best is not None:
+            r = best
+            c.move(r.path(c.pos), r.time)
+            r.ok = True
+            done = True
+        cars.task_done()
+        if done:
+            cars.put(c)
+
+    for car in all_cars:
+        cars.put(car)
+## Second part
     while not cars.empty():
         c = cars.get()
         done = False
