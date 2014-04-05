5d6952a7db726d80a9b2ef7ba1dbfc1a95a293e6
Basile Clement <basile.clement@normale.fr>
2014-04-05 12:53:21 +0200

    Separate

--- routage/parse.py
+++ routage/parse.py
@@ -3,8 +3,7 @@ from Queue import *
 import sys
 import random
 
-GAIN_MULT = 2
-PROFONDEUR = 10
+PROFONDEUR = 1
 
 class voiture(object):
     def __init__(self, inters, rues, pos):
@@ -78,7 +77,7 @@ class rue(object):
         rc = self.real_gain(pos, PROFONDEUR)
         return rc
 
-def trivial(f):
+def parse(f):
     l = f.readline().split(' ')
     ninter = int(l[0])
     nrues = int(l[1])
@@ -103,53 +102,10 @@ def trivial(f):
         r = rue(inters, rue, a, b, d, cost, score)
         rues[ru] = r
     cars = PriorityQueue(nvehic)
-    stuff = set([])
+    all_cars = set([])
     for i in xrange(0, nvehic):
         c = voiture(inters, rues, start)
         cars.put(c)
-        stuff.add(c)
+        all_cars.add(c)
 
-    while not cars.empty():
-        c = cars.get()
-        done = False
-        for r in inters[c.pos].alls:
-            best = None
-            best_cost = None
-            if not r.ok and r.time + c.time < time:
-                cst = r.gain(c.pos)
-                if best is None:
-                    best = r
-                    best_cost = cst
-                elif cst > best_cost:
-                    best = r
-                    best_cost = cst
-        if best is not None:
-            r = best
-            c.move(r.path(c.pos), r.time)
-            r.ok = True
-            done = True
-        if not done:
-            oks = []
-            best_gain = 0.0
-            for r in inters[c.pos].alls:
-                if r.time + c.time < time:
-                    cst = r.gain(c.pos)
-                    if cst * GAIN_MULT > best_gain:
-                        oks.append(r)
-                        if cst > best_gain:
-                            best_gain = cst
-            if len(oks) > 0:
-                r = random.choice(oks)
-                c.move(r.path(c.pos), r.time)
-                done = True
-        cars.task_done()
-        if done:
-            cars.put(c)
-
-    print(nvehic)
-    for c in stuff:
-        print(len(c.path))
-        for i in c.path:
-            print(i)
-
-trivial(sys.stdin)
+    return (cars, inters, rues, time, nvehic, all_cars)
