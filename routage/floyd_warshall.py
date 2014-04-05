f9735519d47ccdadc15ad6f973b2ab239e81d441
Kyo Dralliam <chocobodralliam@gmail.com>
2014-04-05 12:52:15 +0200

    paths added in floyd warshall

--- routage/floyd_warshall.py
+++ routage/floyd_warshall.py
@@ -1,25 +1,38 @@
 def to_matrix(inters, rues):
     costs = []
-    for n in inters:
-        tmp = []
-        for m in inters:
-            if n == m:
-                tmp.append(0)
-            else:
-                tmp.append(+inf)
-        costs.append(tmp)
+    paths = []
 
+    # initialisation
+    for n in inters:
+        tmp_costs = []
+        tmp_paths = []
+        for m in inters:
+            tmp_paths.append([])
+            if n == m:
+                tmp_costs.append(0)
+            else:
+                tmp_costs.append(+inf)
+        costs.append(tmp_costs)
+        paths.append(tmp_paths)
+
+    # filling
     for r in rues:
         costs[r.start][r.end] = r.cost
+        paths[r.start][r.end] = [r]
         if r.bidir:
             costs[r.end][r.start] = r.cost
-    return costs
+            paths[r.start][r.end] = [r]
 
-def floyd_warshall(costs):
+    return (costs, paths)
+
+def floyd_warshall(costs, paths):
     n = len(costs)
     for k in xrange(0,n):
         for i in xrange(0,n):
             for j in xrange(0,n):
-                costs[i][j] = min(costs[i][j], costs[i][k] + costs[k][j])
-    return costs
+                c = costs[i][k] + costs[k][j]
+                if costs[i][j] > c:
+                    costs[i][j] = k
+                    paths[i][j] = paths[i][k] + paths[k][j]
+    return (costs, paths)
 
